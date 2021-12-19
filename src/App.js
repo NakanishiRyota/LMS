c3143f00023527efb30eb1e2a22275e978b21b7e
NakanishiRyota <ryota083124@gmail.com>
2021-12-19 13:37:07 +0900

    メインとテキストを変更 	modified:   src/App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          変更を加える場合は、src/App.jsを編集してください
         </p>
         <a
           className="App-link"
